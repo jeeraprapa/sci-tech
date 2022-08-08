9b46e7fd0fe9e44b844b95069a7ba5eccf663f5a
jeeraprapa <jeeraprapa.h@gmail.com>
2022-08-08 23:40:30 +0700

    merge conflict

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{env('WEB_TITLE','SCI-TECH')}}</title>
-    <link href="https://fonts.googleapis.com/css2?family=K2D:wght@400;600;700&display=swap" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'K2D', sans-serif;
-        }
-    </style>
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <link href="{{asset('vendors/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{mix('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
     <div class="wrapper">
