952aa09b43c5f565dce7f3591928fe3b52793402
P-Chalisa <chalisa.pink@gmail.com>
2022-08-12 02:57:44 +0700

    update css blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -22,11 +22,11 @@
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#">ตำราและกิจกรรมการเรียนการสอน</a>
-                            <a class="nav-link" href="#">ความเชี่ยวชาญเฉพาะด้านของคณาจารย์</a>
-                            <a class="nav-link" href="#">งานวิจัยของนักเรียนและนักศึกษา</a>
-                            <a class="nav-link" href="#">กิจกรรมบริการวิชาการสังคม</a>
+                        <div class="navbar-nav row w-100">
+                            <div class="col"><a class="nav-link active" aria-current="page" href="#">ตำราและกิจกรรมการเรียนการสอน</a></div>
+                            <div class="col"><a class="nav-link" href="#">ความเชี่ยวชาญเฉพาะด้านของคณาจารย์</a></div>
+                            <div class="col"><a class="nav-link" href="#">งานวิจัยของนักเรียนและนักศึกษา</a></div>
+                            <div class="col"><a class="nav-link" href="#">กิจกรรมบริการวิชาการสังคม</a></div>
                         </div>
                     </div>
                 </div>
@@ -35,7 +35,9 @@
         <main>
             <section id="blog">
                 <div class="container-fluid">
-                    <div class="row align-items-start p-5">
+                    <div class="text-header">กิจกรรมบริการวิชาการสังคม</div>
+
+                    <div class="row align-items-start content">
                         <div class="col-12 col-lg-3 p-4">
                             <div class="card" data-bs-toggle="modal" data-bs-target="#modal-blog">
                                 <div class="card-image">
@@ -115,19 +117,21 @@
             <!-- Modal -->
             <div class="modal fade" id="modal-blog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg modal-dialog-scrollable">
-                    <div class="modal-content rounded-sm rounded-3">
-                        <div class="modal-body px-0">
-                            <div class="titletext px-3">
-                                <h5 class="modal-title" id="exampleModalLabel">บริการวิชาการแก่สังคม โครงการพัฒนา คุณภาพชีวิตผู้สูงอายุ</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="modal-content">
+                        <div class="modal-body p-0">
+                            <div class="text-title">
+                                <div class="title">
+                                    <div class=" titletext" id="exampleModalLabel">บริการวิชาการแก่สังคม โครงการพัฒนา คุณภาพชีวิตผู้สูงอายุ</div>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="date">12/02/2564</div>
                             </div>
-                            <div class="date px-3 py-3">12/02/2564</div>
 
-                            <div class="modal-image-content">
+                            <div class="image-content">
                                 <img src="{{asset('images/detail-blog1.png')}}" alt="" class="w-100">
                             </div>
 
-                            <div class="modal-text">
+                            <div class="text-detail">
                                 โครงการบริการวิชาการแก่สังคม ระดับมหาวิทยาลัย พื้นที่เทศบาลตำบลท่าศาลา สำนักวิจัยและบริการวิชาการและคณะวิชา มหาวิทยาลัยพายัพ ร่วมกับเทศบาลตำบลท่าศาลา ดำเนินโครงการบริการวิชาการ ระดับมหาวิทยาลัย ปีการศึกษา 2563 ภายใต้ “โครงการพัฒนาคุณภาพชีวิตผู้สูงอายุ พื้นที่เทศบาลตำบลท่าศาลา อำเภอเมือง จังหวัดเชียงใหม่” โดยมีกลุ่มเป้าหมาย : โรงเรียนเกษียณวัย โดยดำเนินกิจกรรม ในวันที่ 5 กุมภาพันธ์ 2564 เรื่อง “การทำดอกไม้จากผ้าใยบัว เพื่อสร้างรายได้เสริม” วิทยากรและทีมงานจากคณะบริหารธุรกิจและวิทยาศาสตร์ โครงการบริการวิชาการแก่สังคม ระดับมหาวิทยาลัย พื้นที่เทศบาลตำบลท่าศาลา สำนักวิจัยและบริการวิชาการและคณะวิชา มหาวิทยาลัยพายัพ ร่วมกับเทศบาลตำบลท่าศาลา ดำเนินโครงการบริการวิชาการ ระดับมหาวิทยาลัย ปีการศึกษา 2563 ภายใต้
                                 โครงการบริการวิชาการแก่สังคม ระดับมหาวิทยาลัย พื้นที่เทศบาลตำบลท่าศาลา สำนักวิจัยและบริการวิชาการและคณะวิชา มหาวิทยาลัยพายัพ ร่วมกับเทศบาลตำบลท่าศาลา ดำเนินโครงการบริการวิชาการ ระดับมหาวิทยาลัย ปีการศึกษา 2563 ภายใต้ “โครงการพัฒนาคุณภาพชีวิตผู้สูงอายุ พื้นที่เทศบาลตำบลท่าศาลา อำเภอเมือง จังหวัดเชียงใหม่” โดยมีกลุ่มเป้าหมาย : โรงเรียนเกษียณวัย โดยดำเนินกิจกรรม ในวันที่ 5 กุมภาพันธ์ 2564 เรื่อง “การทำดอกไม้จากผ้าใยบัว เพื่อสร้างรายได้เสริม” วิทยากรและทีมงานจากคณะบริหารธุรกิจและวิทยาศาสตร์
                             </div>
