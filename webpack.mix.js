170770b52ed61bde0fe21e619da70ec3b36f52f6
jeeraprapa <jeeraprapa.h@gmail.com>
2022-08-15 01:16:34 +0700

    fix sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/css/app.sass', 'public/css', [
-        //
-    ]);
+    .sass('resources/css/app.sass', 'public/css').options({processCssUrls: false})
+    .sourceMaps().version();
