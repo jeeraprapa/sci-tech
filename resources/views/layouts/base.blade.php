4e7b287ceb9eb1b6e92f629caf17c8faa9ca6499
jeeraprapa <jeeraprapa.h@gmail.com>
2022-08-29 23:53:36 +0700

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -23,5 +23,18 @@
 </body>
 
 <script src="{{asset('vendors/js/bootstrap.bundle.min.js')}}"></script>
+<script>
+    var modal = document.getElementById('modal-video')
+    modal.addEventListener('hidden.bs.modal', function (event) {
+        $('#modal-video').find('iframe').each(function (i){
+            var iframe = modal.getElementsByTagName("iframe")[i];
+            var url = iframe.getAttribute('src');
+            iframe.setAttribute('src', '');
+            iframe.setAttribute('src', url);
+        })
+    })
+
+
+</script>
 
 </html>
