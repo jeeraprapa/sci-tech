ebeb281b138eeb114b3241c8aae5811ab6d38189
jeeraprapa <jeeraprapa.h@gmail.com>
2022-10-05 22:53:30 +0700

    Update _btn.blade.php

--- resources/views/booth/detail/_btn.blade.php
+++ resources/views/booth/detail/_btn.blade.php
@@ -1,24 +1,24 @@
 @if($major->video)
     <div id="video" data-bs-toggle="modal" data-bs-target="#modal-video">
-        <img src="{{asset('images/booth/bt-vdo.png')}}" alt="" class="w-100">
+        <img src="{{asset('images/booth/bt-vdo.png')}}" alt="">
     </div>
 @endif
 
 @if($major->detail)
     <div id="course" data-bs-toggle="modal" data-bs-target="#modal-course">
-        <img src="{{asset('images/booth/bt-course.png')}}" alt="" class="w-100">
+        <img src="{{asset('images/booth/bt-course.png')}}" alt="">
     </div>
 @endif
 
 @if($major->full_name)
     <div id="name" data-bs-toggle="modal" data-bs-target="#modal-name">
-        <img src="{{asset('images/booth/bt-major.png')}}" alt="" class="w-100">
+        <img src="{{asset('images/booth/bt-major.png')}}" alt="">
     </div>
 @endif
 
 @if($major->getMedia('posters')->count())
     <div id="posters" onclick="showGallery()">
-        <img src="{{asset('images/booth/bt-news.png')}}" alt="" class="w-100">
+        <img src="{{asset('images/booth/bt-news.png')}}" alt="">
     </div>
     <div id="lightgallery" class="d-none">
         @foreach($major->getMedia('posters') as $poster)
